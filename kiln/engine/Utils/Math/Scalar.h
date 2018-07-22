e19aa57712e4edc4b7f35dbce8c34a8c1b52cbd4
drakemain <demain1990@gmail.com>
2018-07-22 03:00:31 -0700

    normalize and map

--- kiln/engine/Utils/Math/Scalar.h
+++ kiln/engine/Utils/Math/Scalar.h
@@ -3,8 +3,21 @@
 namespace KMath {
   namespace Scalar {
     template<class T>
-    T lerp(T a, T b, float normal) {
+    inline T lerp(T a, T b, float normal) {
       return T(a + (b - a) * normal);
+    }
+
+    template<class T>
+    inline float norm(T a, T b, T val) {
+      return ((val - a) / (b - a)).f;
+    }
+
+    template<class T>
+    inline T map(T srcA, T srcB, T srcVal, T destA, T destB) {
+      return T(KMath::Scalar::lerp(
+        KMath::Scalar::norm(srcA, srcB, srcVal),
+        destA, destB
+      ));
     }
   };
 };
